c025ea3b26dcb61f85ffac508f50dd875bc167af
stac <staciarimta@gmail.com>
2022-01-26 12:31:31 +0700

    add timeout and max time

--- people-counting/logging.sh
+++ people-counting/logging.sh
@@ -5,5 +5,5 @@ sudo echo "$1,$2,$3" >> /var/log/ppl-log.csv
 # Convert last entry of CSV to JSON
 sudo echo "{'ts':$1,'values':{'R':$2,'L':$3}}" > /var/log/ppl-payload.json 
 
-curl -v -X POST -d @/var/log/ppl-payload.json --url $(cat /home/flash/sc-draco.url) --header "Content-Type:application/json"
+curl -v --connect-timeout 10 -m 20 -X POST -d @/var/log/ppl-payload.json --url $(cat /home/flash/sc-draco.url) --header "Content-Type:application/json"
 
